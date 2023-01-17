eb0fe803fb060ea751dfad4326c49ca20a62fa87
Esolder <40249820+Esolder@users.noreply.github.com>
2023-01-17 14:24:09 +0300

    Избавился от лишних аргументов

--- publish_vk.py
+++ publish_vk.py
@@ -6,15 +6,11 @@ from urllib.parse import urljoin
 from download_comics import download_random_comic
 
 
-
 API_VERSION = 5.131
-BASE_VK_URL = 'https://api.vk.com/method/'
 
 
-def get_upload_url(base_vk_url,
-                   access_token,
-                   group_id,
-                   api_version=API_VERSION):
+def get_upload_url(access_token, group_id, api_version=API_VERSION):
+    base_vk_url = 'https://api.vk.com/method/'
     path = 'photos.getWallUploadServer'
     params = {'access_token': access_token,
               'group_id': group_id,
@@ -34,13 +30,13 @@ def upload_img(upload_url, image_path):
         return json_object['server'], json_object['photo'], json_object['hash']
 
 
-def save_photo(base_vk_url,
-               access_token,
+def save_photo(access_token,
                group_id,
                server,
                photo,
                hash,
                api_version=API_VERSION):
+    base_vk_url = 'https://api.vk.com/method/'
     path = 'photos.saveWallPhoto'
     params = {'access_token': access_token,
               'server': server,
@@ -54,13 +50,13 @@ def save_photo(base_vk_url,
     return photo['id'], photo['owner_id']
 
 
-def publish_photo(base_vk_url,
-                  access_token,
+def publish_photo(access_token,
                   owner_id,
                   message,
                   attachments,
                   from_group=1,
                   api_version=API_VERSION):
+    base_vk_url = 'https://api.vk.com/method/'
     path = 'wall.post'
     params = {'access_token': access_token,
               'owner_id': owner_id,
@@ -80,13 +76,12 @@ if __name__ == '__main__':
     while True:
         try:
             image_path, comment = download_random_comic()
-            upload_url = get_upload_url(BASE_VK_URL, ACCESS_TOKEN, GROUP_ID)
+            upload_url = get_upload_url(ACCESS_TOKEN, GROUP_ID)
             server, photo, hash = upload_img(upload_url, image_path)
-            photo_id, owner_id = save_photo(BASE_VK_URL, ACCESS_TOKEN,
-                                            GROUP_ID, server, photo, hash)
+            photo_id, owner_id = save_photo(ACCESS_TOKEN, GROUP_ID, 
+                                            server, photo, hash)
             attachment = 'photo' + str(owner_id) + '_' + str(photo_id)
-            publish_photo(BASE_VK_URL, ACCESS_TOKEN, "-" +
-                        GROUP_ID, comment, attachment)
+            publish_photo(ACCESS_TOKEN, "-" + GROUP_ID, comment, attachment)
             os.remove(image_path)
             break
         except requests.ConnectionError:
